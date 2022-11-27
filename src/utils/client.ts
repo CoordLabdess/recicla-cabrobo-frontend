215033a34470f3caa363160e13ee38cd622faf57
Lucas Moreira <lucasguilheme.31@gmail.com>
2022-11-27 09:26:35 -0300

    Bugfix: Added login persist

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -1,5 +1,5 @@
 export const PROD_URL = 'https://recicla-cabrobo-backend.herokuapp.com'
 export const DEV_URL = 'https://develop-recicla-cabrobo.herokuapp.com'
 
-// export const CLIENT_URL = PROD_URL
-export const CLIENT_URL = DEV_URL
+export const CLIENT_URL = PROD_URL
+// export const CLIENT_URL = DEV_URL
